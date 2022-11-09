062257d33e7ceae26e705fb5441079bad6c203a3
Skenvy <>
2022-11-10 03:50:01 +1100

    Some non-obvious bug is blocking the file_fetcher_class initialise on line 86, reporting 1 arg given and expected 0

--- lib/dependabot/linguist/local_repo.rb
+++ lib/dependabot/linguist/local_repo.rb
@@ -3,6 +3,8 @@
 require "rugged"
 require "linguist/repository"
 require "dependabot"
+require "dependabot/source"
+require "dependabot/file_fetchers"
 # require "dependabot/file_fetchers"
 require "dependabot/bundler"
 require "dependabot/cargo"
@@ -29,7 +31,7 @@ module Dependabot
     # LocalRepo substitutes for Linguist::Repository and Dependabot::Source
     class LocalRepo
       def initialize(repo_path, repo_name)
-        @repo_path = repo_path
+        @repo_path = repo_path.delete_suffix('/').chomp
         @repo_name = repo_name
         @repo = Rugged::Repository.new(@repo_path)
         @linguist = ::Linguist::Repository.new(@repo, @repo.head.target_id)
@@ -62,6 +64,36 @@ module Dependabot
           end
         end
         @package_ecosystem_to_dependabot_file_fetcher_classes
+      end
+
+      def subfolders
+        # /**/*/ rather than /**/ would remove the base path, but delete_prefix will also remove it, so it needs to be specially added.
+        @subfolders ||= (['/'] | Dir.glob("#{@repo_path}/**/*/").map { |subpath| subpath.delete_prefix(@repo_path).delete_suffix('/') })
+      end
+
+      def sources
+        @sources ||= subfolders.collect { |subfolder| Dependabot::Source.new(provider: 'github', repo: @repo_name, directory: subfolder) }
+      end
+
+      def ecosystems_that_file_fetcher_fetches_files_for
+        @ecosystems_that_file_fetcher_fetches_files_for ||= nil
+        if @ecosystems_that_file_fetcher_fetches_files_for.nil?
+          @ecosystems_that_file_fetcher_fetches_files_for = {}
+          package_ecosystem_to_dependabot_file_fetcher_classes.each do |pacakge_ecosystem, file_fetcher_class|
+            paths_that_were_found_to_have_fetchable_files = []
+            sources.each do |source|
+              puts "spawning class instance for #{pacakge_ecosystem} at #{source.directory}, in repo #{@repo_path}, for class #{file_fetcher_class}"
+              fetcher = file_fetcher_class.new(source: source, credentials: [], repo_contents_path: @repo_path)
+              unless fetcher.files.map(&:name).empty?
+                paths_that_were_found_to_have_fetchable_files |= source.directory
+              end
+            end
+            unless paths_that_were_found_to_have_fetchable_files.empty?
+              ecosystems_that_file_fetcher_fetches_files_for[pacakge_ecosystem] = paths_that_were_found_to_have_fetchable_files
+            end
+          end
+        end
+        @ecosystems_that_file_fetcher_fetches_files_for
       end
     end
   end
