226a7f4199a30e14858c76ef784be37e38cab8bf
Skenvy <>
2023-05-11 21:29:47 +1000

    Remove base patch

--- lib/dependabot/linguist/dependabot_patch.rb
+++ lib/dependabot/linguist/dependabot_patch.rb
@@ -3,6 +3,5 @@
 # Direct the requiring of the files that patch dependabot via this.
 # https://github.com/dependabot/dependabot-core/tree/v0.217.0
 
-require_relative "file_fetchers/base"
 require_relative "file_fetchers/go_modules"
 require_relative "file_fetchers/git_submodules"
